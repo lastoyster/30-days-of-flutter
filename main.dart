2be5bbc6755a80274fad1eb7d086d96f9d3c95eb
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2023-12-16 11:38:37 +0530

    Added Navigation rail UI

--- main.dart
+++ main.dart
@@ -1,6 +1,4 @@
-import 'package:confirmation_slider_example/success.dart';
 import 'package:flutter/material.dart';
-import 'package:slide_to_confirm/slide_to_confirm.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,60 +26,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String title = 'Confirmation Slider';
+  String title = 'Navigation Rail';
+  int _selectedIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
         centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Row(
         children: <Widget>[
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+          NavigationRail(
+            leading: FlutterLogo(),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.lock),
+            ),
+            elevation: 10.0,
+            backgroundColor: Color(0xFFC41A3B),
+            // minWidth: 100.0,
+            minExtendedWidth: 150.0,
+            extended: true,
+            selectedIconTheme: IconThemeData(
+              color: Color(0xFFFBE0E6),
+            ),
+            unselectedIconTheme: IconThemeData(
+              color: Color(0xFF1B1F32),
+            ),
+            selectedLabelTextStyle: TextStyle(
+              color: Color(0xFFFBE0E6),
+            ),
+            unselectedLabelTextStyle: TextStyle(
+              color: Color(0xFF1B1F32),
+            ),
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.none, //by default none
+            // groupAlignment: 0.0, // by default -1.0
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                selectedIcon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.category),
+                selectedIcon: Icon(Icons.category),
+                label: Text('Category'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.search),
+                selectedIcon: Icon(Icons.search),
+                label: Text('Search'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.shopping_cart),
+                selectedIcon: Icon(Icons.add_shopping_cart),
+                label: Text('Cart'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.person_outline),
+                selectedIcon: Icon(Icons.person),
+                label: Text('Profile'),
+              ),
+            ],
+          ),
+          VerticalDivider(
+            width: 2.0,
+            thickness: 2.0,
+            color: Color(0xFFC41A3B),
+          ),
+          Expanded(
+            child: Center(
               child: Text(
-                'Total Amount: \$48',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                'Page ${_selectedIndex + 1}',
+                style: TextStyle(fontSize: 42.0),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ConfirmationSlider(
-              height: 64.0, // by default 70.0
-              width: 300.0, // by default 300.0
-              backgroundColor: Color(0xFFFBE0E6), // by default white
-              // backgroundShape: BorderRadius.circular(0.0), // by default circular
-              foregroundColor: Color(0xFFC41A3B), // by default blueAccent
-              // foregroundShape: BorderRadius.circular(0.0), // by default circular
-              // icon: Icons.arrow_forward, //by default chevron_right
-              iconColor: Color(0xFFFBE0E6), //by default white
-              text: 'Slide to pay', // by default slide to confirm
-              textStyle: TextStyle(
-                fontWeight: FontWeight.bold, // by default bold
-                color: Colors.grey, // by default black26
-                fontSize: 18.0,
-              ),
-              // shadow: BoxShadow(
-              //   color: Colors.black, // by default black38
-              //   offset: Offset(0,4), // by default (0,2)
-              //   blurRadius: 4.0, // by default 2.0
-              //   spreadRadius: 8.0 // by default 0
-              // ),
-              onConfirmation: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Success(),
-                ),
-              ),
-            ),
-          )
         ],
       ),
     );
